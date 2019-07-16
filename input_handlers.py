9b1cb4d0c733da3c7b79685083352baf59ac478b
NekkidBear <52714446+NekkidBear@users.noreply.github.com>
2019-07-16 15:58:17 -0500

    Add files via upload

---
+++ input_handlers.py
@@ -0,0 +1,23 @@
+import libtcodpy as libtcod
+
+
+def handle_keys(key):
+    #movement keys
+    if key.vk == libtcod.KEY_UP:
+        return {'move': (0,-1)}
+    elif key.vk == libtcod.KEY_DOWN:
+        return {'move': (0,1)}
+    elif key.vk == libtcod.KEY_LEFT:
+        return {'move': (-1,0)}
+    elif key.vk == libtcod.KEY_RIGHT:
+        return {'move': (1,0)}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        #alt+enter: toggle full screen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        #exit the game
+        return {'exit': true}
+
+    #no key was pressed
+    return {}
---
+++ map_objects/game_map.py
@@ -0,0 +1,35 @@
+from map_objects.rectangle import Rect
+from map_objects.tile import Tile
+
+
+class GameMap:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.tiles = self.initialize_tiles()
+
+    def initialize_tiles(self):
+        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
+
+    def make_map(self):
+        # Create two rooms for demonstration purposes
+        room1 = Rect(20, 15, 10, 15)
+        room2 = rect(35, 15, 10, 15)
+
+        self.create_room(room1)
+        self.create_room(room2)
+
+
+    def create_room(self, room):
+        # go through the tiles in the rectangle and make them passable
+        for x in range(room.x1+1, room.x2):
+            for y in range(room.y1+1, room.y2):
+                self.tiles[x][y].blocked = False
+                self.tiles[x][y].block_sight = False
+        return tiles
+
+    def is_blocked(self, x, y):
+        if self.tiles[x][y].blocked:
+            return True
+
+        return False
